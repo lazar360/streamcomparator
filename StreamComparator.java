79983564b839104e301c62b2e8d4bd6c50dbea9c
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-04-29 19:04:06 +0200

    add nearest to 0 method

--- StreamComparator.java
+++ StreamComparator.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class StreamComparator {
@@ -11,13 +12,36 @@ public class StreamComparator {
             add(2);
             add(5);
             add(3);
-            add(48);
+            add(-48);
             add(95);
-            add(33);
+            add(-33);
             add(26);
         }};
 
+        // Collection size
+        //System.out.println((long) numbers.size());
 
+        /**
+         * Natural sort is ascending order (1, 2, 3, ...)
+         */
+        /*numbers.stream()
+                .peek(number -> System.out.println("Natural sort: " + number))// Peek is used to print the stream and debug
+                .limit(5);// Limit is used to print only the first 5 elements*/
+
+        /**
+         * Reverse sort is descending order (95, 48, 33, 26, 5)
+         */
+        /*numbers.stream().sorted(Comparator.reverseOrder())
+                        .forEach(number -> System.out.println("Reverse sort: " + number));*/
+
+
+        /**
+         * Sort by number nearest to 0
+         */
+        numbers.stream()
+                        .filter(number -> number > 0)
+                        .min(Comparator.naturalOrder())
+                        .ifPresent(number -> System.out.println("min number: " + number));
 
     }
 }
