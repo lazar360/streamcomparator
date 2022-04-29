ec7a09d25edc7e3f8645569b18b7471dc267cff3
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-04-29 12:23:05 +0200

    first commit

---
+++ StreamComparator.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamComparator {
+
+    public static void main(String[] args) {
+
+        // Create a list of Integers
+        List<Integer> numbers = new ArrayList<>(){{
+            add(1);
+            add(2);
+            add(5);
+            add(3);
+            add(48);
+            add(95);
+            add(33);
+            add(26);
+        }};
+
+
+
+    }
+}
